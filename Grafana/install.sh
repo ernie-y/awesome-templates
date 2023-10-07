77a25af9eac1d191e448a62f0d741f58d1755008
Ernest Yuen <ernestyuen08@gmail.com>
2023-10-07 22:57:21 +0000

    Add grafana setup shell script

---
+++ Grafana/install.sh
@@ -0,0 +1,5 @@
+sudo apt-get install -y gnupg2 curl
+curl https://packages.grafana.com/gpg.key | sudo apt-key add -
+sudo add-apt-repository "deb https://packages.grafana.com/oss/deb stable main"
+sudo apt-get update && sudo apt-get -y install grafana
+sudo service grafana-server start
